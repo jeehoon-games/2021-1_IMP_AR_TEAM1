bab60aff06b77ad85709b16d5b78d2f69a07cf9a
hong3883 <hong3883@naver.com>
2021-03-25 18:01:54 +0900

    Update HelloWorld.cs

--- YutGameAR/Assets/Scripts/HelloWorld.cs
+++ YutGameAR/Assets/Scripts/HelloWorld.cs
@@ -7,12 +7,9 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-<<<<<<< HEAD
+        Debug.Log("Hello World2");
         Debug.Log("Hello World3");
         Debug.Log("Hello World4");
-=======
-        Debug.Log("Hello World2");
->>>>>>> 87e7b8d2fa15b42f34e6812f159c578314f8191c
     }
 
     // Update is called once per frame
